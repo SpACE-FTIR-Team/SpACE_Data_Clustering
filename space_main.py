53d1712fb49093e545596aa4826237c2641d648f
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-09-16 01:08:42 -0500

    Create space_main.py placeholder for main app

---
+++ space_main.py
@@ -0,0 +1,11 @@
+# What kind of standardized header comments do we want to include?
+#
+
+APP_NAME	= "SpACE"
+APP_VERSION	= "0.0.1"
+
+def main():
+	print("This is %s version %s." % (APP_NAME, APP_VERSION))
+
+if __name__ == '__main__':
+    main()
