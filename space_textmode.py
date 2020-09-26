0d07f6af37148a8ffbed3d017d5a1f2f008179d7
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-09-25 20:53:37 -0500

    Create space_textmode.py

---
+++ space_textmode.py
@@ -0,0 +1,47 @@
+#
+# Text mode for SpACE for testing and development
+#
+
+import space_file_ops as fileops
+import space_data_ops as dataops
+
+def one_dobj(folder):
+    """Get one pandas dataframe.
+    Accepts a path to the data files. Parses the first file found in that path
+    that matches the filespec (tir, nicolet, specturm, .txt).
+    Returns one pandas dataframe."""
+    filtered_file_list = get_file_list(folder)
+    if filtered_file_list == None:
+        return None
+    print("Using first file found: %s" % filtered_file_list[0])
+    # parse
+    df = dataops.file_to_data_object(filtered_file_list[0:1])
+    print("Parsed into %s pandas dataframes" % len(df))
+    return df[0]
+
+def all_dobj(folder):
+    """Get all pandas dataframes.
+    Accepts a path to the data files. Parses all the files found in that path
+    that match the filespec (tir, nicolet, specturm, .txt).
+    Returns a list of pandas dataframes."""
+    filtered_file_list = get_file_list(folder)
+    if filtered_file_list == None:
+        return None
+    # parse
+    df = dataops.file_to_data_object(filtered_file_list)
+    print("Parsed into %s pandas dataframes" % len(df))
+    return df
+
+def get_file_list(folder):
+    # verify we have a good path
+    if not fileops.path_exists(folder):
+        # this is a fatal error - log to console
+        print("Invalid path: %s" % folder)
+        return None
+    # search for all files in input folder and subfolders
+    file_list = fileops.collect_all_filenames(folder)
+    print("Found %s files and folders in %s" % (len(file_list), folder))
+    # filter by filename
+    filtered_file_list = fileops.filter_filenames(file_list)
+    print("Found %s files matching the filename filter criteria" % len(filtered_file_list))
+    return filtered_file_list
