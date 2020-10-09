5ac227b3bdf9c50cf7fa68af459b1f27e68dcc7b
sjn5900 <Nack59@live.missouristate.edu>
2020-10-08 20:11:32 -0500

    added basic file saving functionality

--- space_file_ops.py
+++ space_file_ops.py
@@ -29,3 +29,12 @@ def filter_filenames(file_list):
 								and '.txt' in filename,
 								file_list))
 
+def save_files(folder, suffix, data_objects):
+	"""Accepts a specified filepath, a suffix to add to it, and a list of data_objects.  Iterates
+	through them all and saves them to the filepath with the suffix added on. Saves as .csv"""
+	dir_name = folder + suffix
+	if not os.path.isdir(dir_name):
+		os.mkdir(dir_name)
+	for dobj in data_objects:
+		save_string = (dir_name + dobj.filename).rstrip("txt") + "csv"
+		dobj.pairs.to_csv(save_string)  # other arguments can be supplied, check pandas docs
