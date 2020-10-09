b019f83b1e8a45734cea8ddf883649663bfe1084
Collin <tinen0516@live.missouristate.edu>
2020-10-08 20:21:35 -0500

    Added linear normalization from 0 to 1. Implemented PCA on the combined DataFrame. PyCharm formatted some things.
    Signed-off-by: Collin <tinen0516@live.missouristate.edu>

--- normalization.py
+++ normalization.py
@@ -2,16 +2,24 @@
 
 import pandas as pd
 import space_preprocess_test
+import space_data_ops as dataops
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 
-def PCAnormalize(dataObjectArray, dimensions):
-    dataOb = dataObjectArray
-    dims = dimensions
+def linear_normalize(dataObjectArray):
+    scaler = preprocessing.MinMaxScaler()
+    for i in dataObjectArray:
+        normalized_pairs = scaler.fit_transform(i.pairs)
+        n = i.pairs.columns[0]
+        i.pairs.drop(n, axis=1, inplace=True)
+        i.pairs[n] = normalized_pairs
+    return dataObjectArray
 
-    for i in dataOb:
-        pca = PCA(n_components=dims, copy=False, svd_solver='full')
-        pca.fit(i.pairs)
-    #print("PCA done")
+
+def PCAnormalize(dataObjectArray, dimensions):
+    pca = PCA(n_components=dimensions, copy=False, svd_solver='full')
+    pca.fit(dataObjectArray)
+    transformed = pca.transform(dataObjectArray)
+    return transformed
