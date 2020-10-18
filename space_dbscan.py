463c63c2240956a9da11ead8c09c8df27b380abc
austi <austinddrez@gmail.com>
2020-10-18 18:14:12 -0500

    2D Plotting works with live data

--- space_dbscan.py
+++ space_dbscan.py
@@ -2,33 +2,36 @@ print(__doc__)
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pylab as pl
+from matplotlib import cm
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
-from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
 
 
-# Generate sample data
-centers = [[1, 1], [-1, -1], [1, -1]]
-dataset, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
-                            random_state=0)
-
-dataset = StandardScaler().fit_transform(dataset)
 
 # #############################################################################
 # Compute DBSCAN
-def do_dbscan(dataset):
-      db = DBSCAN(eps=0.3, min_samples=10).fit(dataset)
+def do_dbscan(epsilon, minimum, dataset):
+      dataset = StandardScaler().fit_transform(dataset)
+      db = DBSCAN(eps=epsilon, min_samples=minimum).fit(dataset)
+      return db
+      
 
 # #############################################################################
-# Plot result
-       
-      y_pred = db.fit_predict(dataset)
-      plt.scatter(dataset[:,0],
-      dataset[:,1], c = y_pred, cmap='Paired')
-      plt.title("DBSCAN")
-      plt.show()
+# Plot 2D result
+def plot2D(db, dataset):
+      labels = db.labels_
+      colors = [plt.cm.Spectral(each)
+                  for each in np.linspace(0, 1, len(labels))]
+      
+      cx = []
+      cy = []
 
-if __name__ == "__main__":
-      do_dbscan(dataset)
+      for i in labels:
+            cx.append(0)
+            cy.append(1)
+      plt.scatter(x=dataset[0], y=dataset[1], c=labels, cmap='tab20')
+      plt.scatter(x=cx, y=cy, color="black", s=50)
+  
+      plt.show() 
