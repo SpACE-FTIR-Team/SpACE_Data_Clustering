a60ac2ac5ea13640663258c6876a5927fedbcb37
cdtinen <vkt0516@live.missouristate.edu>
2020-10-23 19:43:08 -0500

    2D plotting for DBSCAN fixed. Reformatting.
    Signed-off-by: cdtinen <vkt0516@live.missouristate.edu>

--- space_dbscan.py
+++ space_dbscan.py
@@ -1,56 +1,59 @@
-print(__doc__)
-
-import numpy as np
 import matplotlib.pyplot as plt
-import pylab as pl
-from matplotlib import cm
+import numpy as np
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 
 
-
-# #############################################################################
 # Compute DBSCAN
 def do_dbscan(epsilon, minimum, dataset):
-      dataset = StandardScaler().fit_transform(dataset)
-      db = DBSCAN(eps=epsilon, min_samples=minimum).fit(dataset)
-      return db
-      
+    dataset = StandardScaler().fit_transform(dataset)
+    db = DBSCAN(eps=epsilon, min_samples=minimum).fit(dataset)
+    return db
 
-# #############################################################################
+
 # Plot 2D result
 def plot2D(db, dataset):
-      labels = db.labels_
-      colors = [plt.cm.Spectral(each)
-                  for each in np.linspace(0, 1, len(labels))]
-      
-      cx = []
-      cy = []
+    db = db.fit(dataset)  # refit to reduced data for plotting
+    labels = db.labels_  # dbscan labels represent the cluster each index belongs to
+    unique_labels = set(labels)  # get number of unique labels (equal to number of clusters + 1 for noise)
+    colors = [plt.cm.Spectral(each)  # colors for each cluster and noise
+              for each in np.linspace(0, 1, len(unique_labels))]
+    samples_mask = np.zeros_like(db.labels_, dtype=bool)  # create an array of booleans to represent the labels
+    samples_mask[db.core_sample_indices_] = True  # match booleans to index of clusters
+    set_np = dataset.to_numpy()  # DataFrame to NumPy array conversion for index slicing
 
-      for i in labels:
-            cx.append(0)
-            cy.append(1)
-      plt.scatter(x=dataset[0], y=dataset[1], c=labels, cmap='tab20')
-      plt.scatter(x=cx, y=cy, color="black", s=50)
-  
-      plt.show() 
+    for k, color in zip(unique_labels, colors):
+        if k == -1:
+            color = [0, 0, 0, 1]  # black for noise
 
-# #############################################################################
-# Plot 3D result
+        class_mask = (labels == k)  # array of booleans to match clusters
+
+        xy = set_np[class_mask & samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(color),
+                 markeredgecolor='k', markersize=14)
+
+        # ~samples_mask = samples_mask inverted
+        xy = set_np[class_mask & ~samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(color),
+                 markeredgecolor='k', markersize=6)
+
+    plt.title('DBSCAN')
+    plt.show()
+
+
+# Plot 3D result - not functional
 def plot3D(db, dataset):
-      figure, axes = plt.subplots(subplot_kw={"projection": "3d"})
-      labels = db.labels_
-      cx = []
-      cy = []
-      cz = []
+    figure, axes = plt.subplots(subplot_kw={"projection": "3d"})
+    labels = db.labels_
+    cx = []
+    cy = []
+    cz = []
 
-      for i in labels:
-            cx.append(0)
-            cy.append(1)
-            cz.append(2)
-      axes.scatter3D(xs=dataset[0],ys=dataset[1], zs=dataset[2],
-                    c=labels, cmap="tab20")
-      axes.scatter3D(xs=cx, ys=cy, zs=cz, color="black", s=50)
+    for i in labels:
+        cx.append(0)
+        cy.append(1)
+        cz.append(2)
+    axes.scatter3D(xs=dataset[0], ys=dataset[1], zs=dataset[2],
+                   c=labels, cmap="tab20")
 
-      plt.show() 
+    plt.show()
