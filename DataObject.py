570067fb213e51b73020597394b2d941d4089c54
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-11-23 02:57:11 -0600

    standardize header for dataobject

--- DataObject.py
+++ DataObject.py
@@ -1,8 +1,41 @@
+# -*- coding: utf-8 -*-
+# 
+# Spectral Analysis Clustering Explorer (SpACE)
+# Missouri State University
+# CSC450 Fall 2020 - Dr. Razib Iqbal
 #
-# This class is a container object for each data file.
+# Team 2 (FTIR/ECOSTRESS/SpACE team):
+# Austin Alvidrez
+# Brad Meyer
+# Collin Tinen
+# Kegan Moore
+# Sam Nack
+#
+# Copyright 2020 Austin Alvidrez, Brad Meyer, Collin Tinen,
+# Kegan Moore, Sam Nack
+#
+# Spectral Analysis Clustering Explorer (SpACE) is free software:
+# you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Spectral Analysis Clustering Explorer (SpACE) is distributed in
+# the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Spectral Analysis Clustering Explorer (SpACE).
+# If not, see <https://www.gnu.org/licenses/>.
+
+# DataObject.py
+# This is a class definition for the data object.
+#
+# Each input file is represented as one data object.
 # descriptive_data and xy_pairs are pandas DataFrames
 # filepath is the filepath in form ("ecostress_data_files\\datafile.txt")
-#
 
 class DataObject:
     def __init__(self, descriptive_data, xy_pairs, filepath):
