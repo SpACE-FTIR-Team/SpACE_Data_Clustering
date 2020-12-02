10496e2bb99449f759624c84a4f89fe0e18734ac
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-12-02 01:10:14 -0600

    bump version number

--- space_main.py
+++ space_main.py
@@ -41,7 +41,7 @@ import space_gui as gui
 
 CONFIG = {
     "APP_NAME": "SpACE",
-    "APP_VERSION": "1.0.0rc1",
+    "APP_VERSION": "1.0.0rc2",
     "PCA_BY_DEFAULT": True,
     "DEFAULT_PCA_DIMENSIONS": 2,
     "SAVE_AFTER_DATA_MODIFICATION_BY_DEFAULT": False,
